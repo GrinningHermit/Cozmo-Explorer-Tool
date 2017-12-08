3cfb1df8b4b1d25dd74ccab93fa9602693472eb1
AntonOlivier <anton.olivier@clockwork.nl>
2017-12-08 20:57:34 +0100

    bug fix
    EvtObjectAvailable error fixed

--- event_monitor.py
+++ event_monitor.py
@@ -133,7 +133,7 @@ dispatch_table = {
   cozmo.behavior.EvtBehaviorStopped    : monitor_generic,
   cozmo.anim.EvtAnimationsLoaded       : monitor_generic,
   cozmo.anim.EvtAnimationCompleted     : monitor_EvtActionCompleted,
-  cozmo.objects.EvtObjectAvailable     : monitor_generic,
+  # cozmo.objects.EvtObjectAvailable     : monitor_generic, # deprecated
   cozmo.objects.EvtObjectAppeared      : monitor_generic,
   cozmo.objects.EvtObjectDisappeared   : monitor_generic,
   cozmo.objects.EvtObjectObserved      : monitor_generic,
